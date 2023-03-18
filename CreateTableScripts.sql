d301bd2d1e2173b6aceb99fde31311e8bbf569b1
VanmathiSubramanian <n01503324@humber.ca>
2023-03-17 20:39:38 -0400

    - Create domain classes and tables

---
+++ CreateTableScripts.sql
@@ -0,0 +1,58 @@
+﻿CREATE TABLE User (
+    UserId INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(50) NOT NULL,
+    Email NVARCHAR(100) NOT NULL
+);
+
+CREATE TABLE SalesPerson (
+    SalesPersonId INT IDENTITY(1,1) PRIMARY KEY,
+    SalesPersonName NVARCHAR(50) NOT NULL,
+    SalesPersonEmail NVARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Appointment (
+AppointmentId INT IDENTITY (1, 1) PRIMARY KEY,
+SalesPersonId INT NOT NULL,
+UserId INT NULL,
+AppointmentDate DATETIME NOT NULL,
+AvailabilityStatus VARCHAR(50) NOT NULL,
+AppointmentType VARCHAR(50) NOT NULL,
+CONSTRAINT FK_Appointment_SalesPerson FOREIGN KEY (SalesPersonId) REFERENCES SalesPerson(SalesPersonId),
+CONSTRAINT FK_Appointment_User FOREIGN KEY (UserId) REFERENCES User
+);
+
+CREATE TABLE [Car] (
+CarId INT IDENTITY (1, 1) NOT NULL,
+ModelName VARCHAR(50) NOT NULL,
+ModelNumber INT NOT NULL,
+AvailabilityStatus VARCHAR(50) NOT NULL,
+ModelYear INT NOT NULL,
+CONSTRAINT PK_Car PRIMARY KEY (CarId)
+);
+
+CREATE TABLE [TestDrive] (
+TestDriveId INT IDENTITY (1, 1) NOT NULL,
+TestDriveDate DATETIME NOT NULL,
+CarId INT NOT NULL,
+CONSTRAINT PK_TestDrive PRIMARY KEY (TestDriveId),
+CONSTRAINT FK_TestDrive_Car FOREIGN KEY (CarId) REFERENCES Car(CarId)
+);
+
+CREATE TABLE [Order] (
+OrderId INT IDENTITY (1, 1) NOT NULL,
+CarId INT NOT NULL,
+SalesPersonId INT NOT NULL,
+UserId INT NOT NULL,
+DeliveryDate DATETIME NOT NULL,
+OrderStatus VARCHAR(50) NOT NULL,
+CONSTRAINT PK_Order PRIMARY KEY (OrderId),
+CONSTRAINT FK_Order_Car FOREIGN KEY (CarId) REFERENCES Car(CarId),
+CONSTRAINT FK_Order_SalesPerson FOREIGN KEY (SalesPersonId) REFERENCES SalesPerson(SalesPersonId),
+CONSTRAINT FK_Order_User FOREIGN KEY (UserId) REFERENCES User
+);
+
+
+
+
+
+
--- OrderBot/Order.cs
+++ OrderBot/Order.cs
@@ -2,50 +2,337 @@ using Microsoft.Data.Sqlite;
 
 namespace OrderBot
 {
-    public class Order : ISQLModel
+    public class User : ISQLModel
     {
-        private string _size = String.Empty;
-        private string _phone = String.Empty;
+        private int _userId = -1;
+        private string _name = string.Empty;
+        private string _email = string.Empty;
 
-        public string Phone{
-            get => _phone;
-            set => _phone = value;
+        public int UserId
+        {
+            get { return _userId; }
+            set { _userId = value; }
         }
 
-        public string Size{
-            get => _size;
-            set => _size = value;
+        public string Name
+        { 
+            get { return _name; } 
+            set { _name = value; } 
         }
 
-        public void Save(){
-           using (var connection = new SqliteConnection(DB.GetConnectionString()))
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public void Save()
+        {
+            if (_userId == -1)
             {
-                connection.Open();
+                // create query to insert data
+                string insertQuery = "INSERT INTO User (Name, Email) VALUES (@Name, @Email);";
 
-                var commandUpdate = connection.CreateCommand();
-                commandUpdate.CommandText =
-                @"
-        UPDATE orders
-        SET size = $size
-        WHERE phone = $phone
-    ";
-                commandUpdate.Parameters.AddWithValue("$size", Size);
-                commandUpdate.Parameters.AddWithValue("$phone", Phone);
-                int nRows = commandUpdate.ExecuteNonQuery();
-                if(nRows == 0){
+                // create connection object
+                using (var connection = new SqliteConnection(DB.GetConnectionString()))
+                {
                     var commandInsert = connection.CreateCommand();
-                    commandInsert.CommandText =
-                    @"
-            INSERT INTO orders(size, phone)
-            VALUES($size, $phone)
-        ";
-                    commandInsert.Parameters.AddWithValue("$size", Size);
-                    commandInsert.Parameters.AddWithValue("$phone", Phone);
-                    int nRowsInserted = commandInsert.ExecuteNonQuery();
-
+                    commandInsert.CommandText = insertQuery;
+                    commandInsert.Parameters.AddWithValue("@Name", Name);
+                    commandInsert.Parameters.AddWithValue("@Email", Email);
+                    connection.Open();
+                    commandInsert.ExecuteNonQuery();
                 }
             }
+            else
+            {
+                // create query to insert data
+                string updateQuery = "UPDATE User SET Name = @Name, Email = @Email WHERE UserId = @UserId)";
 
+                // create connection object
+                using (var connection = new SqliteConnection(DB.GetConnectionString()))
+                {
+                    var commandInsert = connection.CreateCommand();
+                    commandInsert.CommandText = updateQuery;
+                    commandInsert.Parameters.AddWithValue("@Name", Name);
+                    commandInsert.Parameters.AddWithValue("@Email", Email);
+                    commandInsert.Parameters.AddWithValue("@UserId", UserId);
+                    connection.Open();
+                    commandInsert.ExecuteNonQuery();
+                }
+            }
+        }
+
+    }    
+    public class SalesPerson : ISQLModel
+    {
+        private int _salesPersonId = -1;
+        private string _salesPersonName = string.Empty;
+        private string _salesPersonEmail = string.Empty;
+
+        public int SalesPersonId
+        {
+            get { return _salesPersonId; }
+            set { _salesPersonId = value; }
+        }
+
+        public string SalesPersonName
+        {
+            get { return _salesPersonName; }
+            set { _salesPersonName = value; }
+        }
+
+        public string SalesPersonEmail
+        {
+            get { return _salesPersonEmail; }
+            set { _salesPersonEmail = value; }
+        }
+        
+        public void Save()
+        {
+            if (_salesPersonId == -1)
+            {
+                // create query to insert data
+                string insertQuery = "INSERT INTO SalesPerson (SalesPersonName, SalesPersonEmail) VALUES (@Name, @Email);";
+
+                // create connection object
+                using (var connection = new SqliteConnection(DB.GetConnectionString()))
+                {
+                    var commandInsert = connection.CreateCommand();
+                    commandInsert.CommandText = insertQuery;
+                    commandInsert.Parameters.AddWithValue("@Name", SalesPersonName);
+                    commandInsert.Parameters.AddWithValue("@Email", SalesPersonEmail);
+                    connection.Open();
+                    commandInsert.ExecuteNonQuery();
+                }
+            }
+            else
+            {
+                // create query to update data
+                string updateQuery = "UPDATE SalesPerson SET SalesPersonName = @Name, SalesPersonEmail = @Email WHERE SalesPersonId = @SalesPersonId)";
+
+                // create connection object
+                using (var connection = new SqliteConnection(DB.GetConnectionString()))
+                {
+                    var commandInsert = connection.CreateCommand();
+                    commandInsert.CommandText = updateQuery;
+                    commandInsert.Parameters.AddWithValue("@Name", SalesPersonName);
+                    commandInsert.Parameters.AddWithValue("@Email", SalesPersonEmail);
+                    commandInsert.Parameters.AddWithValue("@SalesPersonId", SalesPersonId);
+                    connection.Open();
+                    commandInsert.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+    public class Appointment : ISQLModel
+    {
+        private int _appointmentId = -1;
+        private int _salesPersonId = 0;
+        private int _userId = 0;
+        private DateTime _appointmentDate = DateTime.MinValue;
+        private string _availabilityStatus = String.Empty;
+        private string _appointmentType = String.Empty;
+
+        public int AppointmentId
+        {
+            get { return _appointmentId; }
+            set { _appointmentId = value; }
+        }
+
+        public int SalesPersonId
+        {
+            get { return _salesPersonId; }
+            set { _salesPersonId = value; }
+        }
+
+        public int UserId
+        {
+            get { return _userId; }
+            set { _userId = value; }
+        }
+
+        public DateTime AppointmentDate
+        {
+            get { return _appointmentDate; }
+            set { _appointmentDate = value; }
+        }
+
+        public string AvailabilityStatus
+        {
+            get { return _availabilityStatus; }
+            set { _availabilityStatus = value; }
+        }
+
+        public string AppointmentType
+        {
+            get { return _appointmentType; }
+            set { _appointmentType = value; }
+        }
+
+        public void Save()
+        {
+            // create query to update data
+            string updateQuery = "UPDATE Appointment SET UserId = @UserId WHERE AppointmentId = @AppointmentId)";
+
+            // create connection object
+            using (var connection = new SqliteConnection(DB.GetConnectionString()))
+            {
+                var commandInsert = connection.CreateCommand();
+                commandInsert.CommandText = updateQuery;
+                commandInsert.Parameters.AddWithValue("@UserId", UserId);
+                commandInsert.Parameters.AddWithValue("@AppointmentId", AppointmentId);
+                connection.Open();
+                commandInsert.ExecuteNonQuery();
+            }
+        }
+    }
+    public class Car : ISQLModel
+    {
+        private int _carId;
+        private string _modelName;
+        private int _modelNumber;
+        private string _availabilityStatus;
+        private int _modelYear;
+
+        public int CarId
+        {
+            get { return _carId; }
+            set { _carId = value; }
+        }
+
+        public string ModelName
+        {
+            get { return _modelName; }
+            set { _modelName = value; }
+        }
+
+        public int ModelNumber
+        {
+            get { return _modelNumber; }
+            set { _modelNumber = value; }
+        }
+
+        public string AvailabilityStatus
+        {
+            get { return _availabilityStatus; }
+            set { _availabilityStatus = value; }
+        }
+
+        public int ModelYear
+        {
+            get { return _modelYear; }
+            set { _modelYear = value; }
+        }
+
+        public Car(int carId, string modelName, int modelNumber, string availabilityStatus, int modelYear)
+        {
+            _carId = carId;
+            _modelName = modelName;
+            _modelNumber = modelNumber;
+            _availabilityStatus = availabilityStatus;
+            _modelYear = modelYear;
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException("User Cannot Save or Update a CAR. It is inventory data");
+        }
+    }
+    public class TestDrive : ISQLModel
+    {
+        private int _testDriveId;
+        private DateTime _testDriveDate;
+        private int _carId;
+
+        public int TestDriveId
+        {
+            get { return _testDriveId; }
+            set { _testDriveId = value; }
+        }
+
+        public DateTime TestDriveDate
+        {
+            get { return _testDriveDate; }
+            set { _testDriveDate = value; }
+        }
+
+        public int CarId
+        {
+            get { return _carId; }
+            set { _carId = value; }
+        }
+
+        public TestDrive(int testDriveId, DateTime testDriveDate, int carId)
+        {
+            _testDriveId = testDriveId;
+            _testDriveDate = testDriveDate;
+            _carId = carId;
+        }
+
+        public void Save()
+        {
+            string insertQuery = "INSERT INTO TestDrive (TestDriveDate, CarId) VALUES (@TestDriveDate, @CarId);";
+
+            // create connection object
+            using (var connection = new SqliteConnection(DB.GetConnectionString()))
+            {
+                var commandInsert = connection.CreateCommand();
+                commandInsert.CommandText = insertQuery;
+                commandInsert.Parameters.AddWithValue("@TestDriveDate", TestDriveDate);
+                commandInsert.Parameters.AddWithValue("@CarId", CarId);
+                connection.Open();
+                commandInsert.ExecuteNonQuery();
+            }
+        }
+    }
+    public class Order : ISQLModel
+    {
+        private int _orderId;
+        private int _carId;
+        private int _salesPersonId;
+        private int _userId;
+        private DateTime _deliveryDate;
+        private string _orderStatus;
+
+        public int OrderId
+        {
+            get { return _orderId; }
+            set { _orderId = value; }
+        }
+
+        public int CarId
+        {
+            get { return _carId; }
+            set { _carId = value; }
+        }
+
+        public int SalesPersonId
+        {
+            get { return _salesPersonId; }
+            set { _salesPersonId = value; }
+        }
+
+        public int UserId
+        {
+            get { return _userId; }
+            set { _userId = value; }
+        }
+
+        public DateTime DeliveryDate
+        {
+            get { return _deliveryDate; }
+            set { _deliveryDate = value; }
+        }
+        public string OrderStatus
+        {
+            get { return _orderStatus; }
+            set { _orderStatus = value; }
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException("There is no use case for the user to insert or update order data. User can only query status for existing orders");
         }
     }
 }
